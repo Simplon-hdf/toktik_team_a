7ccb71aa53186719927e8b377211466f63e0b165
TheChaosStar <caron.donovan1@outlook.fr>
2023-07-10 12:51:24 +0200

    create config

--- api/main.py
+++ api/main.py
@@ -1,36 +1,9 @@
-from fastapi import Depends, FastAPI, HTTPException
-from sqlalchemy.orm import Session
-from typing import Union
+from fastapi import FastAPI
+from config import engine
 
-# from . import crud, models, schemas
-from .database.database import SessionLocal, engine
+#router imports
 
-
-
-# Create all schemas on startup
-# schemas.Base.metadata.create_all(bind=engine)
+# models imports
 
 # Declare the app
 app = FastAPI()
-
-
-
-# DB session handling
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
