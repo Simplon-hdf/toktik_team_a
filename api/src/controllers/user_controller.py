aa9cdc0d48f2ec8636601310f8a595a94d0a525f
Andhromede <andhromede@hotmail.fr>
2023-07-11 11:51:31 +0200

    modification static methods in user controller

--- api/src/controllers/user_controller.py
+++ api/src/controllers/user_controller.py
@@ -14,26 +14,26 @@ class UserController :
 
     @staticmethod
     def is_password(password: str, user:User) -> bool: 
-    return Hasher.is_valid_password(password, user.password)
+        return Hasher.is_valid_password(password, user.password)
 
     @staticmethod
     def login(db: Session, email: str, password: str) -> str | None:
-    _user = db.query(User).filter(User.email == email).first()
-    if not is_password(db, _user):
-        return None
+        _user = db.query(User).filter(User.email == email).first()
+        if not UserController.is_password(db, _user):
+            return None
 
-    token = _user.token
-    try :
-        jwt.decode( token, SECURE_KEY, algorithms = ["HS256"] )
-    except jwt.ExpiredSignatureError: 
-        token = generate_token(db, email, password)
-    return token
+        token = _user.token
+        try :
+            jwt.decode( token, SECURE_KEY, algorithms = ["HS256"] )
+        except jwt.ExpiredSignatureError: 
+            token = UserController.generate_token(db, email, password)
+        return token
 
     # generate token for existant user
     @staticmethod
     def generate_token(db: Session, email: str, password: str) -> str | None:
         _user = db.query(User).filter(User.email == email).first()
-        if not is_password(db, _user):
+        if not UserController.is_password(db, _user):
             return None
         token = jwt.encode({ "exp": datetime.now(tz=timezone.utc) }, SECURE_KEY)
         _user.token = token
@@ -45,10 +45,10 @@ class UserController :
     def register(db: Session, user: UserSchema) -> str | None:
         hashed_password = Hasher.get_password_hash(user.password)
         regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(.[A-Z|a-z]{2,})+')
-    
+
         if not re.fullmatch(regex, user.email):
             return None
-        
+
         token = jwt.encode({ "exp": datetime.now(tz=timezone.utc) }, SECURE_KEY)
         _user = User(
             username = user.username,
@@ -63,29 +63,29 @@ class UserController :
 
     @staticmethod
     def update_user(db: Session, user:dict, token:str) -> bool :
-    _user = get_user_by_token(db, token)
-    if _user == None :
-        return False
-    
-    if user["email"] is not None :
-        regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(.[A-Z|a-z]{2,})+')
-        if not re.fullmatch(regex, user["email"]):
+        _user = UserController.get_user_by_token(db, token)
+        if _user == None :
             return False
-        _user.email = user["email"]
 
-    if user["password"] is not None :
-        _user.password = Hasher.get_password_hash(user["password"])
+        if user["email"] is not None :
+            regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(.[A-Z|a-z]{2,})+')
+            if not re.fullmatch(regex, user["email"]):
+                return False
+            _user.email = user["email"]
 
-    if user["username"] is not None :
-        _user.username = user["username"]
+        if user["password"] is not None :
+            _user.password = Hasher.get_password_hash(user["password"])
 
-    db.commit()
-    db.refresh(_user)
-    return True
+        if user["username"] is not None :
+            _user.username = user["username"]
+
+        db.commit()
+        db.refresh(_user)
+        return True
 
     @staticmethod
     def delete_user(db: Session, token:str) :
-    _user = get_user_by_token(db, token)
-    db.delete(_user)
-    db.commit()
-    return { 'message': "User is deleted" }
+        _user = UserController.get_user_by_token(db, token)
+        db.delete(_user)
+        db.commit()
+        return { 'message': "User is deleted" }
