2a861d21977934e716c0d97fb02779276f8e28cf
Vincent Clipet <vincent.clipet.7@gmail.com>
2023-07-12 10:45:53 +0100

    feat: added CORS handling, everything is allowed for now

--- api/src/main.py
+++ api/src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .config import engine
 from .routers import CommentRouter, PostRouter, UserRouter
@@ -11,7 +12,17 @@ UserSchema.metadata.create_all(bind=engine)
 # Declare the app
 app = FastAPI()
 
+# Open routes
 app.include_router(UserRouter.router)
 app.include_router(PostRouter.router)
 app.include_router(CommentRouter.router)
 
+# CORS
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
